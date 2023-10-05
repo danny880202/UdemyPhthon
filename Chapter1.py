eddec914eb8165b0a5d73d33e22a9929cba365a4
danny880202 <danny0912861956@gmail.com>
2023-10-05 13:50:13 +0800

    2023/10/05

--- Chapter1.py
+++ Chapter1.py
@@ -130,3 +130,84 @@ x = [1, 2, [4, 5, 6], 2, 1, 5, 3, [4, 3, [-10, 4]]]
 print(x[2])
 print(x[7][2][0])
 print(x[7][2])
+
+# *  Dictionary練習
+person = {"name": "Danny", "age": 25}
+print(person["name"])
+print(person["age"])
+
+person = {"x": {"age": [10, 20, 40]}}  # Dictionary中的Dictionary，或者在之中加入list
+# & 一個Dictionary(KEY欄位)，他的(value欄位)可以有很多的Dictionary，也可以有很多的List 。
+
+print(person["x"]["age"][2])
+
+x = {}  # 另一種Dictionary寫法。給他空白Dictionary，再給他加入資料
+x["name"] = "Danny"
+x["age"] = 28
+x["name"] = "APPlE"  # Dictionary可以 mutable資料
+print(x)
+print(x.keys())
+print(x.values())
+print(x.items())  # 是 key 和 value一對，是一個 list of tuples
+
+# * Tuples 練習
+# Tuples 是一個有序 immutable 的sequence of objects ( LIST )
+myTuple = (10, "100", "hello")
+print(myTuple[0])
+print(myTuple.count(10))  # 10出現幾次?
+
+print(myTuple.index("hello"))  # hello在第幾個index
+
+# * Tuple packing 和 Tuple unpacking
+
+x = 10, 15, 17, 126  # 只要用 逗號 隔開，將會自動變成一個 tuple packing
+print(x)
+print(type(x))  # < class "tuple" >
+
+x = ("Danny", 30)
+name, age = x
+print(name)
+print(age)  # variable names have meanings
+
+x = 67
+y = 72
+
+# temp = x
+# x = y
+# y =temp
+
+x, y = y, x
+# &  tuple unpacking =  tuple packing
+print(x)
+print(y)
+
+a = ([1, 2, 3], "Danny")
+a[0][1] = 100
+print(a)
+
+# * Sets 練習
+# an unordered collection of unique objects
+
+mylist = [5, 1, 4, 2, 3, 6, 1, 3, 5, 2, 4, 7, 6]
+myset = set(mylist)
+print(myset)
+
+s = set()
+s.add(1)
+s.add(2)
+print(s)
+s.add(3)
+s.discard(1)
+print(s)
+
+a = {1, 3, 4, 5}
+b = {3, 4, 7, 8}
+print(a.isdisjoint(b))  # A和B 是否互斥
+print(a.issubset(b))  # A是否為B的子集
+print(a.issuperset(b))  # A是否為B的母集合
+print(a.difference(b))  # A-B
+print(b.difference(a))  # B-A
+print(a.intersection(b))
+print(b.intersection(a))
+print(a.union(b))
+print(b.union(a))
